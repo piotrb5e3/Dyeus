cd0d365e0dfa98b88e4c674c0d16b7b2b37002b7
Piotr Bakalarski <piotrb5e3@gmail.com>
2017-03-10 20:56:35 +0100

    Test users app views

---
+++ users/tests/test_views.py
@@ -0,0 +1,50 @@
+import json
+from faker import Faker
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+
+from appliances.tests.factory import create_appliance
+from sensors.tests.factory import create_sensor
+from users.tests.factory import create_regular_dyeus_user
+
+
+class TestUnauthenticatedUsersViews(APITestCase):
+    user = None
+
+    def setUp(self):
+        self.user = create_regular_dyeus_user()
+
+    def test_fails_auth_test(self):
+        url = reverse('auth-test')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_cant_get_user_data(self):
+        url = reverse('user-data')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class TestAuthenticatedUsersViews(APITestCase):
+    user = None
+
+    def setUp(self):
+        self.user = create_regular_dyeus_user()
+
+        (token, _) = Token.objects.get_or_create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+    def test_passes_auth_test(self):
+        url = reverse('auth-test')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_get_user_data(self):
+        url = reverse('user-data')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content), {
+            'username': self.user.username
+        })
--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from rest_framework.authtoken import views
 from .views import auth_test, get_current_user_data
 
 urlpatterns = [
-    url(r'^auth/gettoken', views.obtain_auth_token),
-    url(r'^auth/test', auth_test),
-    url(r'^user', get_current_user_data),
+    url(r'^auth/gettoken', views.obtain_auth_token, name='auth-get-token'),
+    url(r'^auth/test', auth_test, name='auth-test'),
+    url(r'^user', get_current_user_data, name='user-data'),
 ]
