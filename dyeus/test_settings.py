01ccbb44fd633fbc2f67548c539ce1e66408c6c4
Piotr Bakalarski <piotrb5e3@gmail.com>
2017-03-11 15:25:11 +0100

    Fix test settings

--- dyeus/test_settings.py
+++ dyeus/test_settings.py
@@ -1,6 +1,6 @@
-from production_settings import *
+from .local_settings import *
 
-#DATABASES['default'] = {
-#    'ENGINE': 'django.db.backends.sqlite3',
-#    'NAME': ':memory:',
-#}
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': ':memory:',
+}
