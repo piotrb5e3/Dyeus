2f5db5a0def4fec77c43b4723ecc13e0b3bfd285
Piotr Bakalarski <piotrb5e3@gmail.com>
2017-03-13 23:28:24 +0100

    About page. About is the landing page for unauthenticated users.

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -16,6 +16,7 @@ Router.map(function () {
     this.route('new');
   });
   this.route('logout');
+  this.route('about');
 });
 
 export default Router;
--- frontend/app/routes/index.js
+++ frontend/app/routes/index.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {});
+export default Ember.Route.extend({
+  session: Ember.inject.service('session'),
+  beforeModel(/* transition */) {
+    if (!this.get('session').get('isAuthenticated')) {
+      this.transitionTo('about');
+    }
+  }
+});
